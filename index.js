788faa08b40499ad28fa9c692343d79b6c110c34
rakib2ksa <mrakib.ksa766@gmail.com>
2025-01-16 08:23:30 +0300

    my first website

---
+++ index.js
@@ -0,0 +1,6 @@
+// JavaScript for toggling the navigation menu visibility
+function toggleMenu() {
+    const navList = document.querySelector(".nav-list");
+    // Toggle the 'active' class on the navigation menu
+    navList.classList.toggle("active");
+}
