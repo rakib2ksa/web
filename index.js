e688361a24d331bcde1fa45dc0565bada8f88105
rakib2ksa <mrakib.ksa766@gmail.com>
2025-03-21 18:22:49 +0300

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-// JavaScript for toggling the navigation menu visibility
-function toggleMenu() {
-    const navList = document.querySelector(".nav-list");
-    // Toggle the 'active' class on the navigation menu
-    navList.classList.toggle("active");
-}
